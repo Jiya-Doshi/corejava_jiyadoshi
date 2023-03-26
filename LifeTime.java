14cc3674ef264fa9ee0a55324acfac3d996b878d
Jiya-Doshi <128312838+Jiya-Doshi@users.noreply.github.com>
2023-03-26 22:19:12 +0530

    Add files via upload

---
+++ LifeTime.java
@@ -0,0 +1,16 @@
+// Demonstrate Lifetime of a variable.
+class Lifetime
+{
+    public static void main(String args[])
+   {
+    int x;
+    for(x = 0; x<3; x++)
+    {
+        int y = -1; // y is initialized each time block is entered
+        System.out.println(" y is: " + y);
+        y = 100;
+        System.out.println(" y is now: " + y);
+
+    }
+   }
+}
