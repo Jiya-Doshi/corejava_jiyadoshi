448dbc1e1df38be197a2da6be9fa37852622fcb4
Jiya Doshi <128312838+Jiya-Doshi@users.noreply.github.com>
2023-04-03 21:46:19 +0530

    Add files via upload

---
+++ While.java
@@ -0,0 +1,13 @@
+// Demonstrate while loop.
+class www
+{
+  public static void main(String args[])
+  {
+    int n = 10;
+    while (n > 0) 
+    {
+        System.out.println("tick" + n);
+        n--;
+    }
+  }
+}
