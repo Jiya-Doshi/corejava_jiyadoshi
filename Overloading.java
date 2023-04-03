039c47262990f6270e1b0285188eddc96f301cfa
Jiya Doshi <128312838+Jiya-Doshi@users.noreply.github.com>
2023-04-03 21:47:06 +0530

    Add files via upload

---
+++ Overloading.java
@@ -0,0 +1,20 @@
+class Overloading
+{
+    int s(String a,int b)
+    {
+        System.out.println("a:"+a+","+"b:"+b);
+        return b;
+    }
+   int s(int a,int b)
+   {
+
+   
+    System.out.print("THIS IS INT2");
+   return a+b
+
+   }
+   double s(lond a,int b)
+   {
+    System.out.print("THIS IS INT3");
+    }
+}
