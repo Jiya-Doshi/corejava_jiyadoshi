d655abdb5d4aa74827382efdb81f5dbe840cac33
Jiya Doshi <128312838+Jiya-Doshi@users.noreply.github.com>
2023-04-04 22:59:42 +0530

    Add files via upload

---
+++ Add.java
@@ -0,0 +1,10 @@
+class Add
+{
+    public static void main(String a[]) 
+    {
+        int num1 = 3;
+        int num2 = 5;
+        int result = num1 + num2;
+        System.out.println(result);
+    }
+}
