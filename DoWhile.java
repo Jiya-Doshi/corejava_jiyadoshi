ca965eca35de23cd8f80879a3eef87c848611244
Jiya Doshi <128312838+Jiya-Doshi@users.noreply.github.com>
2023-04-07 23:16:33 +0530

    Add files via upload

---
+++ DoWhile.java
@@ -0,0 +1,14 @@
+// Demonstrate the do-while loop.
+class DoWhile
+{
+    public static void main(String args[])
+    {
+    int n = 10;
+    do
+    {
+        System.out.println("tick " + n);
+        n--;
+
+    } while (--n > 0);
+    }
+}
