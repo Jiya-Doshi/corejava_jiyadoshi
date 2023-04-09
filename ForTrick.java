aa0125530606b36625fe6bf80b8f7da6754d3b59
Jiya Doshi <128312838+Jiya-Doshi@users.noreply.github.com>
2023-04-09 23:26:22 +0530

    Add files via upload

---
+++ ForTrick.java
@@ -0,0 +1,11 @@
+// Demonstrate the for loop.
+class ForTrick
+{
+    public static void main(String args[])
+    {
+        int n;
+        for(n=10; n>0; n--)
+        System.out.println("tick " + n);
+        
+    }
+}
