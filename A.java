74ae29933e24bc6bb57e4c41bb692b0cd6f5e479
Jiya Doshi <128312838+Jiya-Doshi@users.noreply.github.com>
2023-04-13 22:46:11 +0530

    Add files via upload

---
+++ A.java
@@ -0,0 +1,24 @@
+package P;
+class Bicycle
+{
+  // state or field
+  private int gear = 5;
+
+  // behavior or method
+  public void braking() {
+    System.out.println("Working of Braking");
+  }
+}
+class Main
+{
+public static void main(String args[])
+{
+ object = new className();
+
+// for Bicycle class
+Bicycle sportsBicycle = new Bicycle();
+
+Bicycle touringBicycle = new Bicycle();
+}
+}
+
