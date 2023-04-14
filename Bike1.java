0cf256a85604af84c3df2e902001a180693489e8
Jiya Doshi <128312838+Jiya-Doshi@users.noreply.github.com>
2023-04-14 23:19:25 +0530

    Add files via upload

---
+++ Bike1.java
@@ -0,0 +1,19 @@
+// Java Program to create and call a default constructor.
+class Bike1
+{
+    //creating a default constructor.
+    Bike1()
+    {
+        System.out.println("Bike is created");
+    }
+
+
+
+
+    public static void main(String args[])
+    // calling a default constructor
+ {
+
+  Bike1 b = new Bike1();
+ }
+}
