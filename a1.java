19cf39046a2c6e3ecf3088f12b95c5cf7b56b686
Jiya Doshi <128312838+Jiya-Doshi@users.noreply.github.com>
2023-04-14 23:17:49 +0530

    Add files via upload

---
+++ a1.java
@@ -0,0 +1,16 @@
+
+
+public class a1
+{
+    public void s()
+    {
+        System.out.println("This is package program");
+
+    }
+    public static void main(String[] args)
+    {
+        a1 a = new a1();
+        a.s();
+    }
+
+}
