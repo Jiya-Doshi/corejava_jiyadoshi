7af6f138e28f96f689be472e62ff4d90b3b2747d
Jiya Doshi <128312838+Jiya-Doshi@users.noreply.github.com>
2023-04-20 23:20:00 +0530

    Add files via upload

---
+++ CMD2.java
@@ -0,0 +1,10 @@
+class CMD
+{
+    public static void main(String[] args)
+    {
+        System.out.println(args[0]);
+        System.out.println(args[1]);
+        System.out.println(Integer.parseInt(args[0]) + Integer.parseInt(args[1]));
+
+    }
+}
