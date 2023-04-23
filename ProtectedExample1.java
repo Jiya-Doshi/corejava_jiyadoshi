04eb5de4a6ef7750fab743aabbc2ebbc5e498a4c
Jiya Doshi <128312838+Jiya-Doshi@users.noreply.github.com>
2023-04-23 20:51:51 +0530

    Add files via upload

---
+++ ProtectedExample1.java
@@ -0,0 +1,11 @@
+package com.javatpoint;
+import com.java.A;
+
+public class ProtectedExample1
+{
+    public static void main(String[] args)
+    {
+        A a = new A();
+        System.out.println(a.msg);
+    }
+}
