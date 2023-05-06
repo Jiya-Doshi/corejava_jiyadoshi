ca55ed0b3de9adf2755babf0269f41a0696bafe5
Jiya Doshi <128312838+Jiya-Doshi@users.noreply.github.com>
2023-05-06 10:01:15 +0530

    Add files via upload

---
+++ Multi.java
@@ -0,0 +1,13 @@
+class Multi extends Thread
+{
+    public void run()
+        {
+            System.out.println("Thread is running");
+        }
+
+        public static void main(String args[])
+        {
+            Multi t1 = new Multi();
+            t1.start();
+        }
+}
