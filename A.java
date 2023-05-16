728a413092fae65d81bf912f45a522e52b3c5d93
Jiya Doshi <128312838+Jiya-Doshi@users.noreply.github.com>
2023-05-16 22:07:06 +0530

    Add files via upload

--- A.java
+++ A.java
@@ -1,25 +1,14 @@
-// A simple example of Inheritance.
-//Create a superclass.
-class A 
-{
-    int i,j;
+ import java.util.*;  
+ public class A
+{  
+ public static void main(String args[]){  
+  ArrayList<String> list=new ArrayList<String>();//Creating arraylist    
+      list.add("Mango");//Adding object in arraylist    
+      list.add("Apple");    
+      list.add("Banana");    
+      list.add("Grapes");    
+      //Printing the arraylist object   
+      System.out.println(list);  
+ }  
+}  
 
-    void showj()
-    {
-        System.out.println("i and j: " + i + " " + j);
-    }
-}
-
-//Create a subclass by extending class A.
-class B extends A
-{
-    int k;
-    void showk()
-    {
-        System.out.println("k: " + k);
-    }
-    void sum()
-    {
-        System.out.println("i+j+k:" + (i+j+k));
-    }
-}
